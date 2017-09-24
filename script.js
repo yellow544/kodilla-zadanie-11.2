90287a4e0c24473ada0cffe710fa5d98f6be9ffc
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-24 20:29:40 +0100

    js update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function Button(text) {
 Button.prototype = {
 	create: function() {
 		const self = this;					//wskazuje kontekst, zapobiega utracie kontekstu
-		this.$element = $('<button>'); 		//jQuery - tworzy nowe przycsiki na stronie
-		this.$element.text(this.text); 		//jQuery - ustawienie tekstu na przycisku
+		this.$element = $('<button>');		//jQuery - tworzy nowe przycsiki na stronie
+		this.$element.text(this.text);		//jQuery - ustawienie tekstu na przycisku
 		this.$element.click(function() {	//po kliknięciu wyświetla tekst
 			alert(self.text);
 		});
@@ -23,4 +23,5 @@ Button.prototype = {
 const btn1 = new Button('Hello!'); 
 
 //wywołanie metody w celu stworzenia elementu
+
 btn1.create();
