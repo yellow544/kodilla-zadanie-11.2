18af2e0da6c88f9be167498c99226f838cf4f73d
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-25 18:46:47 +0100

    polish comment removed

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-//Utworzenie funkcji konstruującej Button
-
 function Button(text) {
 	this.text = text || 'Hello';
 }
 
-//Utworzenie podstawowej metody create tworzącej przycisk na stronie
-
 Button.prototype = {
 	create: function() {
-		const self = this;					//wskazuje kontekst, zapobiega utracie kontekstu
-		this.$element = $('<button>');		//jQuery - tworzy nowe przycsiki na stronie
-		this.$element.text(this.text);		//jQuery - ustawienie tekstu na przycisku
-		this.$element.click(function() {	//po kliknięciu wyświetla tekst
+		const self = this;					
+		this.$element = $('<button>');		
+		this.$element.text(this.text);
+		this.$element.click(function() {
 			alert(self.text);
 		});
-		this.$element.appendTo($('body')); 	//umieszczenie elementu w DOM - pobiera element i przykleja go do body
+		this.$element.appendTo($('body'));
 	}
 }
 
-//utworzenie pierwszej instancji
-
-const btn1 = new Button('Hello!'); 
-
-//wywołanie metody w celu stworzenia elementu
+var btn1 = new Button('Hello!'); 
 
 btn1.create();
